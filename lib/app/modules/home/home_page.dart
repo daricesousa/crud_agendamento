ad4562694bb5dc74deccc1312c0e978bcb077a02
Darice Sousa <daricesousa@gmail.com>
2023-04-25 14:56:55 -0300

    Add responsibility for displaying appointments

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:crud_agendamento/app/core/utils/formatters.dart';
 import 'package:crud_agendamento/app/modules/home/home_controller.dart';
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:responsive_row/responsive_row.dart';
 
 class HomePage extends GetView<HomeController> {
   const HomePage({Key? key}) : super(key: key);
@@ -14,42 +15,55 @@ class HomePage extends GetView<HomeController> {
         itemCount: controller.appointments.length,
         itemBuilder: (context, index) {
           final appointment = controller.appointments[index];
-          return ListTile(
-            title: Wrap(
-              alignment: WrapAlignment.spaceBetween,
-              children: [
-                Wrap(
+          return ResponsiveCol(
+            child: Card(
+              child: ListTile(
+                title: ResponsiveRow(
+                  alignment: WrapAlignment.spaceBetween,
                   children: [
-                    const Icon(Icons.person),
-                    Text(appointment.name),
+                    itemCard(
+                        icon: Icons.person,
+                        text: appointment.name,
+                        sizes: Sizes.col3),
+                    itemCard(
+                        icon: Icons.calendar_month,
+                        text: Formatters.dateDisplay(appointment.date)),
+                    itemCard(
+                      icon: Icons.timer,
+                      text: Formatters.hourDisplay(appointment.date),
+                    ),
+                    itemCard(
+                        icon: Icons.phone,
+                        text: Formatters.foneDisplay(appointment.fone),
+                        visible: appointment.fone.isNotEmpty),
                   ],
                 ),
-                Wrap(
-                  children: [
-                    const Icon(Icons.calendar_month),
-                    Text(Formatters.dateDisplay(appointment.date)),
-                  ],
-                ),
-                Wrap(
-                  children: [
-                    const Icon(Icons.timer),
-                    Text(Formatters.hourDisplay(appointment.date)),
-                  ],
-                ),
-                Visibility(
-                  visible: appointment.fone.isNotEmpty,
-                  child: Wrap(
-                    children: [
-                      const Icon(Icons.phone),
-                      Text(Formatters.foneDisplay(appointment.fone)),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
           );
         },
       ),
     ));
+  }
+
+  ResponsiveCol itemCard(
+      {IconData? icon,
+      String text = '',
+      bool visible = true,
+      Sizes sizes = Sizes.col2}) {
+    return ResponsiveCol(
+      lg: sizes,
+      md: sizes,
+      sm: Sizes.col12,
+      child: Visibility(
+        visible: visible,
+        child: Row(
+          children: [
+            Icon(icon),
+            Text(text),
+          ],
+        ),
+      ),
+    );
   }
 }
